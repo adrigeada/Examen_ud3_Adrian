be1bfca4604963cff0c405658d6b5de0b6431140
A709\dam1 <adrigeada@gmail.com>
2025-11-27 19:23:52 +0100

    Examen

--- Examen_ud3_Adrian/src/main/java/org/example/Olivas.java
+++ Examen_ud3_Adrian/src/main/java/org/example/Olivas.java
@@ -1,24 +1,38 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Olivas {
     static void main() {
         Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
 
         System.out.println("BIENVENIDO AL OLIVERO");
         System.out.println("Introduce el tamaño de tu bancal");
        int ancho = 0;
        int alto = 0;
+       boolean repetir = false;
 
-        do {
-            System.out.println("Ancho: ");
-            ancho = teclado.nextInt(); //j
-            System.out.println("Alto: ");
-            alto = teclado.nextInt(); //i
+       do {
+           repetir= false;
+           try {
+               System.out.println("Ancho: ");
+               ancho = teclado.nextInt(); //j
+               System.out.println("Alto: ");
+               alto = teclado.nextInt(); //i
 
-        }while (ancho != alto);
+           } catch (Exception e) {
+               System.out.println("Pon numeros");
+               repetir= true;
+               teclado.nextLine();
+           }
+       }while(repetir);
+
+
+
+
 
         int[][] bancal = new int[alto][ancho];
         teclado.nextLine();
@@ -32,6 +46,7 @@ public class Olivas {
             String fila = teclado.nextLine();
             String[] fila_vector = fila.split(" ");
 
+            if (fila_vector.length == ancho){
 
                 for (int j = 0; j < bancal[i].length; j++) {
                     if (fila_vector[j].matches("\\d{1,2}")){
@@ -49,6 +64,14 @@ public class Olivas {
 
 
                 }
+            }else {
+                System.out.println("La longitud de tu hilera tiene que ser igual a la de tu bancal");
+                i--;
+                continue fuera;
+            }
+
+
+
 
 
         }
@@ -82,10 +105,10 @@ public class Olivas {
 
         int resultado_columna= 0;
         float resultado_col_litro= 0;
-        for (int i = 0; i < bancal.length; i++) {
+        for (int i = 0; i < bancal[0].length; i++) {
             resultado_columna= 0;
             resultado_col_litro= 0;
-            for (int j = 0; j < bancal[i].length; j++) {
+            for (int j = 0; j < bancal.length; j++) {
 
                 resultado_columna += bancal[j][i];
 
@@ -100,26 +123,85 @@ public class Olivas {
         System.out.println("Olivos mas productivos");
 
         int aux = 0;
+        int aux1 = 0;
+        int aux2  = 0;
 
         for (int i = 0; i < bancal.length; i++) {
             for (int j = 0; j < bancal[i].length; j++) {
 
                 if (bancal[i][j] > 35){
-                    System.out.println("-En la posicion "+(i) +" "+(j)+" con "+ bancal[i][j]+ " kilos");
+                    System.out.println("-En la posicion "+(i) +" , "+(j)+" con "+ bancal[i][j]+ " kilos");
                 }
 
                 if (bancal[i][j] > aux){
                     aux = bancal[i][j];
+                    aux1=i;
+                    aux2=j;
                 }
 
             }
 
         }
 
-        System.out.println("El arbol mas productivo ha generado "+aux + "KG");
+        System.out.println("El arbol mas productivo ha generado "+aux + "KG en la posicion ["+(aux1+1)+" , " + (aux2+1)+"]");
 
 
 
+        int[][] replantar = new int[alto][ancho];
+
+        for (int i = 0; i < bancal.length; i++) {
+            for (int j = 0; j < bancal[i].length; j++) {
+
+                replantar[i][j] = bancal[i][j];
+
+            }
+
+        }
+        for (int i = 0; i < bancal.length; i++) {
+            for (int j = 0; j < bancal[i].length; j++) {
+
+                if (replantar[i][j] < 5 ){
+                    replantar[i][j] = 1;
+
+                }else {
+                    replantar[i][j]= 0;
+                }
+
+            }
+
+        }
+        System.out.println("=====================");
+        System.out.println("MAPA REPLANTAR (1 = REPLANTAR 0 = NO REPLANTAR)");
+        for (int i = 0; i < bancal.length; i++) {
+            for (int j = 0; j < bancal[i].length; j++) {
+
+                System.out.print(replantar[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = 0; i < bancal.length; i++) {
+            for (int j = 0; j < bancal[i].length; j++) {
+
+                if (bancal[i][j]< 5){
+                    bancal[i][j] = aleatorio.nextInt(21)+15;
+                }
+
+            }
+
+        }
+
+
+        System.out.println("=================");
+        System.out.println("Mapa estimación despues de plantar:");
+        for (int i = 0; i < bancal.length; i++) {
+            for (int j = 0; j < bancal[i].length; j++) {
+
+                System.out.print(bancal[i][j]+ " ");
+            }
+            System.out.println();
+        }
 
 
 
